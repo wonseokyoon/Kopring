efbda5021feeef88e03a6da83df27d019d270e54
wonseokyoon <backend.wonseok@gmail.com>
2025-03-26 12:12:24 +0900

    Migration:initData

--- backend/src/main/java/com/example/upload/global/webMvc/WebMvcConfig.kt
+++ backend/src/main/java/com/example/upload/global/webMvc/WebMvcConfig.kt
@@ -7,8 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 class WebMvcConfig: WebMvcConfigurer {
+
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler("/gen/**")
-                .addResourceLocations("file:///${ AppConfig.getGenFileDirPath() }/");
+            .addResourceLocations("file:///${AppConfig.getGenFileDirPath()}/")
     }
 }
